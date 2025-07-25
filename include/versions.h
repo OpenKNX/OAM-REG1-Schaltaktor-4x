2c088c6f4965ac0a58020badd820d671b66701c6
Ing-Dom <dom@ing-dom.de>
2025-07-25 09:52:56 +0200

    change OFM-SwitchActuator branch

--- include/versions.h
+++ include/versions.h
@@ -1,24 +1,18 @@
 #pragma once
 
-<<<<<<< HEAD
-#define MAIN_Version "b93ca19"
+#define MAIN_Version "4e18ea1"
 #define KNX_Version "2.2.0+4fb43ed"
 #define MODULE_Common_Version "1.4.3+d0c5729"
-=======
-#define MAIN_Version "5c0e534"
-#define KNX_Version "2.2.0+4fb43ed"
-#define MODULE_Common_Version "1.4.3+a9e8644"
->>>>>>> 8a2f2246988bcb15800deb30c91608f7a0e3b1ca
 #define MODULE_Common_Version_Major 1
 #define MODULE_Common_Version_Minor 4
 #define MODULE_Common_Version_Revision 3
 #define MODULE_Common_ETS 20
-#define MODULE_HardwareConfig_Version "1.0.0+259a4ff"
+#define MODULE_HardwareConfig_Version "1.0.0+35e6a92"
 #define MODULE_HardwareConfig_Version_Major 1
 #define MODULE_HardwareConfig_Version_Minor 0
 #define MODULE_HardwareConfig_Version_Revision 0
 #define MODULE_HardwareConfig_ETS 16
-#define MODULE_LogicModule_Version "3.7.1+e89ff5d"
+#define MODULE_LogicModule_Version "3.7.1+1ba35cb"
 #define MODULE_LogicModule_Version_Major 3
 #define MODULE_LogicModule_Version_Minor 7
 #define MODULE_LogicModule_Version_Revision 1
@@ -28,7 +22,7 @@
 #define MODULE_FileTransferModule_Version_Minor 1
 #define MODULE_FileTransferModule_Version_Revision 1
 #define MODULE_FileTransferModule_ETS 1
-#define MODULE_SwitchActuator_Version "0.1.0+b934132"
+#define MODULE_SwitchActuator_Version "0.1.0+95c285a"
 #define MODULE_SwitchActuator_Version_Major 0
 #define MODULE_SwitchActuator_Version_Minor 1
 #define MODULE_SwitchActuator_Version_Revision 0
